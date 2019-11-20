4ee132235a7072094854f854d5458d09bb35f7ba
roxlu <diederick@roxlu.com>
2019-11-20 14:49:01 +0100

    Added feature that allows you to retrieve a texture id from the driver.

--- filament/backend/src/opengl/OpenGLDriver.cpp
+++ filament/backend/src/opengl/OpenGLDriver.cpp
@@ -622,7 +622,6 @@ void OpenGLDriver::createTextureR(Handle<HwTexture> th, SamplerType target, uint
             mPlatform.createExternalImageTexture(t);
         } else {
             glGenTextures(1, &t->gl.id);
-
             t->gl.internalFormat = getInternalFormat(format);
             assert(t->gl.internalFormat);
 
@@ -1104,6 +1103,7 @@ void OpenGLDriver::createStreamFromTextureIdR(Handle<HwStream> sh,
     s->streamType = StreamType::TEXTURE_ID;
     glGenTextures(GLStream::ROUND_ROBIN_TEXTURE_COUNT, s->user_thread.read);
     glGenTextures(GLStream::ROUND_ROBIN_TEXTURE_COUNT, s->user_thread.write);
+
     for (auto& info : s->user_thread.infos) {
         info.ets = mPlatform.createExternalTextureStorage();
     }
@@ -2549,6 +2549,11 @@ void OpenGLDriver::readStreamPixels(Handle<HwStream> sh,
         gl.bindFramebuffer(GL_FRAMEBUFFER, 0);
         scheduleDestroy(std::move(p));
     }
+}
+
+void OpenGLDriver::getTextureId(Handle<HwTexture> th, void* result) {
+  GLTexture* t = handle_cast<GLTexture*>(th);
+  *(uint32_t*)result= t->gl.id;
 }
 
 // ------------------------------------------------------------------------------------------------
